b330c5e997fa90d61a301191a4cefaa2b3be2b6b
Erika Veloso <erikaveloso@msn.com>
2025-04-08 21:14:25 -0300

    aula do dia 08/04

--- src/main/java/br/com/alunoonline/api/controller/AlunoController.java
+++ src/main/java/br/com/alunoonline/api/controller/AlunoController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/alunos")
 public class AlunoController {
@@ -17,7 +20,17 @@ public class AlunoController {
     @ResponseStatus(HttpStatus.CREATED)
     public void criarAluno(@RequestBody Aluno aluno){
         alunoService.criarAluno(aluno);
-
     }
 
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Aluno> listarTodosAlunos(){
+        return alunoService.listarTodosAlunos();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<Aluno> buscarAlunoPorId(@PathVariable Long id){
+        return alunoService.buscarAlunoPorId(id);
+    }
 }
