a939a6a6cb88628f5b4830040052d66449d5f0f9
Erika Veloso <erikaveloso@msn.com>
2025-04-22 21:02:35 -0300

    commit

--- src/main/java/br/com/alunoonline/api/controller/ProfessorController.java
+++ src/main/java/br/com/alunoonline/api/controller/ProfessorController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/professor")
+@RequestMapping("/professores")
 public class ProfessorController {
 
     @Autowired
