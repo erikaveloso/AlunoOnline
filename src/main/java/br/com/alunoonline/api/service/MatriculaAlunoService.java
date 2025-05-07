bba06da33ec19d65ed49b5532189a3b22ce9a79b
Erika Veloso <erikaveloso@msn.com>
2025-05-06 21:01:17 -0300

    commit aula

--- src/main/java/br/com/alunoonline/api/service/MatriculaAlunoService.java
+++ src/main/java/br/com/alunoonline/api/service/MatriculaAlunoService.java
@@ -4,7 +4,9 @@ import br.com.alunoonline.api.enums.MatriculaAlunoStatusEnum;
 import br.com.alunoonline.api.model.MatriculaAluno;
 import br.com.alunoonline.api.repository.MatriculaAlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class MatriculaAlunoService {
@@ -15,5 +17,20 @@ public class MatriculaAlunoService {
     public void criarMatricula(MatriculaAluno matriculaAluno){
         matriculaAluno.setStatus(MatriculaAlunoStatusEnum.MATRICULADO);
         matriculaAlunoRepository.save(matriculaAluno);
+    }
+
+    public void trancarMatricula(Long matriculaAlunoId){
+        // Antes de trancar, verifica se a matrícula existe
+        MatriculaAluno matriculaAluno = matriculaAlunoRepository.findById(matriculaAlunoId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "Matrícula Aluno não encontrada"));
+        // Só vai deixar trancar, se o status ATUAL for MATRICULADO
+        if (matriculaAluno.getStatus().equals(MatriculaAlunoStatusEnum.MATRICULADO)){
+            matriculaAluno.setStatus(MatriculaAlunoStatusEnum.TRANCADO);
+            matriculaAlunoRepository.save(matriculaAluno);
+        } else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "Só é possível trancar com o seu status matriculado.");
+        }
     }
 }
